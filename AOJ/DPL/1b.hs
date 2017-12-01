86936769827ac2c19cb1c359878d221bef95112e
kkisic <amareamo.pxhxc@gmail.com>
2017-12-02 02:14:47 +0900

    AOJ DPL 1b

---
+++ AOJ/DPL/1b.hs
@@ -0,0 +1,41 @@
+import Data.Array.IO
+
+type Value = Int
+type Weight = Int
+type Item = (Weight, Value)
+type ItemList = [Item]
+type Capacity = Int
+
+main :: IO ()
+main = do
+  input <- getContents
+  let nums = map ((\x -> (last x, head x)) . map (read :: String -> Int) . words) . lines $ input
+      capacity = fst . head $ nums
+      items = tail nums
+  memo <- newArray ((0, 0), (capacity, length items)) (0 - 1) :: IO (IOUArray (Int, Int) Int)
+  dp (capacity, items) memo
+  putStrLn . show . last =<< getElems memo
+
+memoize :: ((Capacity, ItemList) -> IOUArray (Int, Int) Int -> IO (IOUArray (Int, Int) Int)) -> (Capacity, ItemList) -> IOUArray (Int, Int) Int -> IO Int
+memoize f (c, i) t = do
+  v <- readArray t (c, length i)
+  if v < 0
+    then do
+      f (c, i) t
+      return =<< readArray t (c, length i)
+    else return v
+
+dp :: (Capacity, ItemList) -> IOUArray (Int, Int) Int -> IO (IOUArray (Int, Int) Int)
+dp (c, []) t = do
+  writeArray t (c, 0) 0
+  return t
+dp (c, as@(i:is)) t
+  | c < fst i = do
+    v <- memoize dp (c, is) t
+    writeArray t (c, length as) v
+    return t
+  | otherwise = do
+    x <- memoize dp (c, is) t
+    y <- memoize dp (c - fst i, is) t
+    writeArray t (c, length as) (max x $ y + snd i)
+    return t
