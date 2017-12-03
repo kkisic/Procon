dafe52b5f7b0efa15466b8fc8bc5e8ff59d4f8aa
kkisic <amareamo.pxhxc@gmail.com>
2017-12-03 23:30:17 +0900

    atcoder abc080b

--- AtCoder/abc/080/b.hs
+++ AtCoder/abc/080/b.hs
@@ -1,11 +1,3 @@
 import Data.Char
-{-
-main = do
-  input <- getLine
-  let x = (read :: String -> Int) input
-      y = sum . map (read :: Char -> Int) $ x
-      -}
-
-
 
 main = interact $ (\x -> if (mod ((read :: String -> Int) x) (sum . map digitToInt . init $ x)) == 0 then "Yes\n" else "No\n")
