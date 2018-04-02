a4a32aa7aab32bf7ce582ad9e1f3ca44aaeb54f0
kkisic <amareamo.pxhxc@gmail.com>
2018-04-02 13:59:17 +0900

    atcoder arc055ab C++

---
+++ CPP/AtCoder/arc/055/a.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+#define int long long int
+
+const int inf = 1e15;
+
+using namespace std;
+
+signed main(){
+    int n;
+    cin >> n;
+    cout << 1;
+    for(int i = 0; i < n-1; i++){
+        cout << 0;
+    }
+    cout << 7 << endl;
+    return 0;
+}
