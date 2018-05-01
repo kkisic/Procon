ee2322a61092cee7315f278c2a7fe78197814520
kkisic <amareamo.pxhxc@gmail.com>
2018-05-01 23:51:23 +0900

    atcoder Dwango4a C++

---
+++ CPP/AtCoder/other/Dwango4/a.cpp
@@ -0,0 +1,32 @@
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <iomanip>
+
+#define int long long int
+#define rep(i, n) for(int i = 0; i < (n); ++i)
+
+using namespace std;
+
+typedef pair<int, int> P;
+
+const int INF = 1e15;
+const int MOD = 1e9+7;
+
+signed main(){
+    string s;
+    cin >> s;
+
+    if(s[0] == s[2] && s[1] == s[3]){
+        cout << "Yes" << endl;
+        return 0;
+    }
+    cout << "No" << endl;
+
+    return 0;
+}
