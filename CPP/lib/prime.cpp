d65839b81325b0a2fde3a7eff5c44d698b08ef78
kkisic <amareamo.pxhxc@gmail.com>
2018-06-05 00:56:33 +0900

    add C++ library prime.cpp

---
+++ CPP/lib/prime.cpp
@@ -0,0 +1,42 @@
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <utility>
+#include <iomanip>
+
+#define int long long int
+
+using namespace std;
+
+typedef pair<int, int> P;
+
+const int INF = 1e15;
+const int MOD = 1e9+7;
+
+vector<int> prime(int n){
+    vector<int> t;
+    vector<bool> x(n+1, true);
+    t.push_back(2);
+
+    int i;
+    for(i = 3; i * i <= n; i += 2){
+        if(!x[i]){
+            continue;
+        }
+        t.push_back(i);
+        for(int j = i; j <= n; j += i){
+            x[j] = false;
+        }
+    }
+    for(; i <= n; i += 2){
+        if(x[i]){
+            t.push_back(i);
+        }
+    }
+}
+
+signed main (){
+    return 0;
+}
