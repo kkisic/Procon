ca218b91ac3a23441cfec5788610202dcd979665
kkisic <amareamo.pxhxc@gmail.com>
2018-06-10 22:54:52 +0900

    atcoder abc099 ab C++

---
+++ CPP/AtCoder/abc/099/b.cpp
@@ -0,0 +1,30 @@
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <iomanip>
+
+#define int long long int
+#define rep(i, n) for(int i = 0; i < (n); ++i)
+
+using namespace std;
+
+typedef pair<int, int> P;
+
+const int INF = 1e15;
+const int MOD = 1e9+7;
+
+signed main(){
+    int a, b;
+    cin >> a >> b;
+    int diff = b - a;
+
+    int ans = (1 + diff) * diff / 2;
+    cout << ans - b << endl;
+
+    return 0;
+}
