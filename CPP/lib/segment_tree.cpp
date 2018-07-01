41324ea208db304e4cf9b2577c519a20ddfe103f
kkisic <amareamo.pxhxc@gmail.com>
2018-07-01 15:04:31 +0900

    add C++ library segment_tree

---
+++ CPP/lib/segment_tree.cpp
@@ -0,0 +1,54 @@
+//Range Minimum Query
+//segment_tree<int> st(L+1, INF, [](int a, int b){return min(a, b);});
+
+//Range Sum Query
+//segment_tree<int> st(L+1, 0, [](int a, int b){return a+b;});
+
+template <typename T>
+class segment_tree{
+    private:
+        static int calc_size(int n){
+            int m = 1;
+            while(m < n){
+                m *= 2;
+            }
+            return m;
+        }
+
+        T query(int s, int t, int i, int l, int r) const {
+            if(r <= s || t <= l){
+                return init;
+            }
+
+            if(s <= l && r <= t){
+                return node[i];
+            }
+
+            int m = l + (r - l) / 2;
+            T vl = query(s, t, i * 2 + 1, l, m);
+            T vr = query(s, t, i * 2 + 2, m, r);
+            return f(vl, vr);
+        }
+
+    public:
+        int n;
+        vector<T> node;
+        T init;
+        function<T(T, T)> f;
+
+        segment_tree(int n, T init, function<T(T, T)> f)
+            : n(calc_size(n)), node(calc_size(n) * 2, init), init(init), f(f){}
+
+        void update(int i, const T& x){
+            i += (n - 1);
+            node[i] = x;
+            while(i > 0){
+                i = (i - 1) / 2;
+                node[i] = f(node[2*i+1], node[2*i+2]);
+            }
+        }
+
+        T query(int s, int t) const {
+            return query(s, t, 0, 0, n);
+        }
+};
