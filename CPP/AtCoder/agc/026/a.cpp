949db11cac278f525e20a48d8d506b3042102df5
kkisic <amareamo.pxhxc@gmail.com>
2018-07-14 23:30:02 +0900

    atcoder agc026 ac C++

---
+++ CPP/AtCoder/agc/026/a.cpp
@@ -0,0 +1,42 @@
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#include <stack>
+#include <tuple>
+#include <bitset>
+#include <algorithm>
+#include <utility>
+#include <iomanip>
+
+#define int long long int
+#define rep(i, n) for(int i = 0; i < (n); ++i)
+
+using namespace std;
+
+typedef pair<int, int> P;
+
+const int INF = 1e15;
+const int MOD = 1e9+7;
+
+signed main(){
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    rep(i, n) cin >> a[i];
+
+    int ans = 0;
+    for(int i = 1; i < n; i++){
+        if(a[i] == a[i-1]){
+            ans++;
+            i++;
+        }
+    }
+    cout << ans << endl;
+
+    return 0;
+}
