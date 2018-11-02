c07ac539d28d91453d179d65e68e6659d5ac3fd6
kkisic <amareamo.pxhxc@gmail.com>
2018-11-02 17:58:52 +0900

    move some files

--- CPP/AtCoder/other/Dwango4F/a.cpp
+++ CPP/AtCoder/other/Dwango4F/a.cpp
@@ -2,8 +2,12 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <deque>
 #include <map>
 #include <set>
+#include <stack>
+#include <tuple>
+#include <bitset>
 #include <algorithm>
 #include <utility>
 #include <iomanip>
@@ -17,16 +21,50 @@ typedef pair<int, int> P;
 
 const int INF = 1e15;
 const int MOD = 1e9+7;
+const int MAX = 120*360;
+const int HR = 1;
+const int MR = 12;
+const int SR = 720;
 
 signed main(){
-    string s;
-    cin >> s;
+    int h, m, s, c1, c2;
+    cin >> h >> m >> s >> c1 >> c2;
 
-    if(s[0] == s[2] && s[1] == s[3]){
-        cout << "Yes" << endl;
+    h = (h % 12) * 60 * 60 + m * 60 + s;
+    m = (m * 60 + s) * MR;
+    s *= SR;
+
+    int d1, d2, count;
+    d1 = d2 = count = 0;
+    int first, last;
+    first = last = 0;
+    while(d1 <= c1 && d2 <= c2){
+        if(m > s && m + MR <= s + SR){
+            d1++;
+        }
+        if(h > m && h + HR <= m + MR){
+            d2++;
+        }
+
+        count++;
+        if(first == 0 && d1 == c1 && d2 == c2){
+            first = count;
+            if(h + HR == m + MR || m + MR == s + SR){
+                first++;
+            }
+        }
+
+        h = (h + HR) % MAX;
+        m = (m + MR) % MAX;
+        s = (s + SR) % MAX;
+    }
+
+    if(first == 0){
+        cout << -1 << endl;
         return 0;
     }
-    cout << "No" << endl;
+
+    cout << first << " " << count - 1 << endl;
 
     return 0;
 }
