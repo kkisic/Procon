c64f0eaf9f1f692d8e9a767eb6fdd0ed305178e9
kkisic <amareamo.pxhxc@gmail.com>
2018-12-01 13:48:55 +0900

    atcoder xmascon17 b C++

---
+++ CPP/AtCoder/other/xmascon17/b.cpp
@@ -0,0 +1,53 @@
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#include <stack>
+#include <tuple>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <utility>
+#include <iomanip>
+
+#define int long long int
+#define rep(i, n) for(int i = 0; i < (n); ++i)
+#define ALL(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define UNIQUE(v) v.erase( unique(v.begin(), v.end()), v.end() )
+
+using namespace std;
+
+typedef pair<int, int> P;
+
+const int INF = 1e15;
+const int MOD = 1e9+7;
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    cout << 16 << " " << 17 << endl;
+
+    cout << "...............X*" << endl;
+    cout << "..............m7." << endl;
+    cout << ".............a1.." << endl;
+    cout << "............s0..." << endl;
+    cout << "...........C2...." << endl;
+    cout << "..........ot....." << endl;
+    cout << ".........ns......" << endl;
+    cout << "........te......." << endl;
+    cout << ".......et........" << endl;
+    cout << "......sn........." << endl;
+    cout << ".....to.........." << endl;
+    cout << "....2C..........." << endl;
+    cout << "...0s............" << endl;
+    cout << "..1a............." << endl;
+    cout << ".7m.............." << endl;
+    cout << "*X..............." << endl;
+
+    return 0;
+}
