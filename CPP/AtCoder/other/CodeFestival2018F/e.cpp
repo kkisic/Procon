1906a0db756f9279d9e89ea3f37fc35336be21f8
kkisic <amareamo.pxhxc@gmail.com>
2018-12-11 12:40:29 +0900

    atcoder CodeFestival2018F e C++

---
+++ CPP/AtCoder/other/CodeFestival2018F/e.cpp
@@ -0,0 +1,51 @@
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#include <stack>
+#include <tuple>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <utility>
+#include <iomanip>
+
+#define int long long int
+#define rep(i, n) for(int i = 0; i < (n); ++i)
+#define ALL(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define UNIQUE(v) v.erase( unique(v.begin(), v.end()), v.end() )
+
+using namespace std;
+
+typedef pair<int, int> P;
+
+const int INF = 1e15;
+const int MOD = 1e9+7;
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    rep(i, n) cin >> a[i];
+
+    priority_queue<P, vector<P>, greater<P>> pq;
+    int ans = 0;
+    rep(i, n){
+        pq.emplace(a[i], i + k);
+
+        while(pq.top().second <= i){
+            pq.pop();
+        }
+        ans += pq.top().first;
+    }
+    cout << ans << endl;
+
+    return 0;
+}
