1c1bc832ba40fd43cc922abe5efa68484c0fa300
kkisic <amareamo.pxhxc@gmail.com>
2018-12-29 23:00:01 +0900

    atcoder agc030 a C++

---
+++ CPP/AtCoder/agc/030/a.cpp
@@ -0,0 +1,42 @@
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#include <stack>
+#include <tuple>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <utility>
+#include <iomanip>
+
+#define int long long int
+#define rep(i, n) for(int i = 0; i < (n); ++i)
+#define ALL(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define UNIQUE(v) v.erase( unique(v.begin(), v.end()), v.end() )
+
+using namespace std;
+
+typedef pair<int, int> P;
+
+const int INF = 1e15;
+const int MOD = 1e9+7;
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    int ans = 0;
+    ans = min(a + b + 1, c);
+    ans += b;
+    cout << ans << endl;
+
+    return 0;
+}
