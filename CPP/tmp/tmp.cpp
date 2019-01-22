2124420a4fedb1eacfc99c37f8d34fcb71a9704a
kkisic <amareamo.pxhxc@gmail.com>
2019-01-23 03:18:52 +0900

    update C++ template file

--- CPP/tmp/tmp.cpp
+++ CPP/tmp/tmp.cpp
@@ -26,6 +26,20 @@ typedef pair<int, int> P;
 const int INF = 1e15;
 const int MOD = 1e9+7;
 
+template <typename T>
+using vector2 = vector<vector<T>>;
+template <typename T>
+vector2<T> initVec2(size_t n0, size_t n1, T e = T()){
+    return vector2<T>(n0, vector<T>(n1, e));
+}
+
+template <typename T>
+using vector3 = vector<vector<vector<T>>>;
+template <typename T>
+vector3<T> initVec3(size_t n0, size_t n1, size_t n2, T e = T()){
+    return vector3<T>(n0, vector2<T>(n1, vector<T>(n2, e)));
+}
+
 signed main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
