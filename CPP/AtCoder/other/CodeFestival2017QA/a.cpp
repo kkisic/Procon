8c692c7bcd2952060ef512e44ffed10d9cea01b2
kkisic <amareamo.pxhxc@gmail.com>
2019-01-22 16:44:12 +0900

    atcoder CodeFestival2017QA a C++

---
+++ CPP/AtCoder/other/CodeFestival2017QA/a.cpp
@@ -0,0 +1,39 @@
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#include <stack>
+#include <tuple>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <utility>
+#include <iomanip>
+
+#define int long long int
+#define rep(i, n) for(int i = 0; i < (n); ++i)
+#define ALL(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define UNIQUE(v) v.erase( unique(v.begin(), v.end()), v.end() )
+
+using namespace std;
+
+typedef pair<int, int> P;
+
+const int INF = 1e15;
+const int MOD = 1e9+7;
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    string s;
+    cin >> s;
+
+    cout << (s.substr(0, 4) == "YAKI" ? "Yes" : "No") << endl;
+
+    return 0;
+}
