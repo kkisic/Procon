102a39e335f087681a5cb96612c5ec00eab7fc59
kkisic <amareamo.pxhxc@gmail.com>
2019-01-27 04:39:05 +0900

    atcoder joi2007sp-day2 b C++

---
+++ CPP/AtCoder/other/joi2007sp-day2/b.cpp
@@ -0,0 +1,72 @@
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#include <stack>
+#include <tuple>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <utility>
+#include <iomanip>
+
+#define rep(i, n) for(int i = 0; i < (n); ++i)
+#define ALL(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define UNIQUE(v) v.erase( unique(v.begin(), v.end()), v.end() )
+
+using namespace std;
+
+typedef pair<int, int> P;
+
+template <typename T>
+using vector2 = vector<vector<T>>;
+template <typename T>
+vector2<T> initVec2(size_t n0, size_t n1, T e = T()){
+    return vector2<T>(n0, vector<T>(n1, e));
+}
+
+template <typename T>
+using vector3 = vector<vector<vector<T>>>;
+template <typename T>
+vector3<T> initVec3(size_t n0, size_t n1, size_t n2, T e = T()){
+    return vector3<T>(n0, vector2<T>(n1, vector<T>(n2, e)));
+}
+
+int MOD;
+int powM(int x, int n){
+    if(n == 0){
+        return 1;
+    }
+    if(n % 2 == 0){
+        return powM(x * x % MOD, n / 2);
+    }
+    return powM(x, n-1) * x % MOD;
+}
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> MOD >> n;
+
+    int x[10000] = {};
+    rep(i, MOD){
+        int a = powM(i, n);
+        x[a]++;
+    }
+
+    long long ans = 0;
+    rep(i, MOD){
+        rep(j, MOD){
+            ans += x[i] * x[j] * x[(i+j)%MOD];
+        }
+    }
+    cout << ans << endl;
+
+    return 0;
+}
