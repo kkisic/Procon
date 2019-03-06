bf42534341b10a52a8a5fdfa30dcfb8db9c53952
kkisic <amareamo.pxhxc@gmail.com>
2019-03-06 17:53:11 +0900

    Codeforces 1132 abcf C++

---
+++ CPP/Codeforces/1132/f.cpp
@@ -0,0 +1,70 @@
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#include <stack>
+#include <tuple>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <utility>
+#include <iomanip>
+
+#define int long long int
+#define rep(i, n) for(int i = 0; i < (n); ++i)
+#define ALL(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define UNIQUE(v) v.erase( unique(v.begin(), v.end()), v.end() )
+
+using namespace std;
+
+typedef pair<int, int> P;
+
+const int INF = 1e15;
+const int MOD = 1e9+7;
+
+template <typename T>
+using vector2 = vector<vector<T>>;
+template <typename T>
+vector2<T> initVec2(size_t n0, size_t n1, T e = T()){
+    return vector2<T>(n0, vector<T>(n1, e));
+}
+
+template <typename T>
+using vector3 = vector<vector<vector<T>>>;
+template <typename T>
+vector3<T> initVec3(size_t n0, size_t n1, size_t n2, T e = T()){
+    return vector3<T>(n0, vector2<T>(n1, vector<T>(n2, e)));
+}
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    string s;
+    cin >> n >> s;
+
+    auto dp = initVec2<int>(n, n, INF);
+    rep(i, n){
+        dp[i][i] = 1;
+    }
+    for(int x = 1; x < n; x++){
+        rep(i, n){
+            int j = min(i + x, n - 1);
+            for(int k = i; k < j; k++){
+                if(s[i] == s[j]){
+                    dp[i][j] = min(dp[i][j], dp[i][k] + dp[k+1][j] - 1);
+                }else{
+                    dp[i][j] = min(dp[i][j], dp[i][k] + dp[k+1][j]);
+                }
+            }
+        }
+    }
+    cout << dp[0][n-1] << endl;
+
+    return 0;
+}
