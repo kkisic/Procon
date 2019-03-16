bfa60943b95611300837b7790836d4c6d71be215
kkisic <amareamo.pxhxc@gmail.com>
2019-03-16 23:46:26 +0900

    atcoder agc031 ab C++

---
+++ CPP/AtCoder/agc/031/b.cpp
@@ -0,0 +1,72 @@
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#include <stack>
+#include <tuple>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <utility>
+#include <iomanip>
+
+#define int long long int
+#define rep(i, n) for(int i = 0; i < (n); ++i)
+#define ALL(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define UNIQUE(v) v.erase( unique(v.begin(), v.end()), v.end() )
+
+using namespace std;
+
+typedef pair<int, int> P;
+
+const int INF = 1e15;
+const int MOD = 1e9+7;
+
+template <typename T>
+using vector2 = vector<vector<T>>;
+template <typename T>
+vector2<T> initVec2(size_t n0, size_t n1, T e = T()){
+    return vector2<T>(n0, vector<T>(n1, e));
+}
+
+template <typename T>
+using vector3 = vector<vector<vector<T>>>;
+template <typename T>
+vector3<T> initVec3(size_t n0, size_t n1, size_t n2, T e = T()){
+    return vector3<T>(n0, vector2<T>(n1, vector<T>(n2, e)));
+}
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    vector<int> c(n);
+    rep(i, n) cin >> c[i];
+
+    vector<int> nxt(n);
+    vector<int> last(2e5+1, INF);
+    for(int i = n - 1; i >= 0; i--){
+        nxt[i] = last[c[i]];
+        last[c[i]] = i;
+    }
+
+    vector<int> dp(n + 1);
+    dp[0] = 1;
+    rep(i, n){
+        dp[i+1] += dp[i];
+        dp[i+1] %= MOD;
+        if(nxt[i] != INF && i + 1 != nxt[i]){
+            dp[nxt[i]] += dp[i];
+            dp[nxt[i]] %= MOD;
+        }
+    }
+    cout << dp[n] << endl;
+
+    return 0;
+}
