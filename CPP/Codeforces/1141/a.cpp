7f667823f5a310c9fa57bcffffdc6aef406839f2
kkisic <amareamo.pxhxc@gmail.com>
2019-05-07 01:17:13 +0900

    Codeforces 1141 abc C++

---
+++ CPP/Codeforces/1141/a.cpp
@@ -0,0 +1,73 @@
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#include <stack>
+#include <tuple>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <utility>
+#include <iomanip>
+
+#define int long long int
+#define rep(i, n) for(int i = 0; i < (n); ++i)
+#define ALL(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define UNIQUE(v) v.erase( unique(v.begin(), v.end()), v.end() )
+
+using namespace std;
+
+typedef pair<int, int> P;
+
+const int INF = 1e15;
+const int MOD = 1e9+7;
+
+template <typename T>
+using vector2 = vector<vector<T>>;
+template <typename T>
+vector2<T> initVec2(size_t n0, size_t n1, T e = T()){
+    return vector2<T>(n0, vector<T>(n1, e));
+}
+
+template <typename T>
+using vector3 = vector<vector<vector<T>>>;
+template <typename T>
+vector3<T> initVec3(size_t n0, size_t n1, size_t n2, T e = T()){
+    return vector3<T>(n0, vector2<T>(n1, vector<T>(n2, e)));
+}
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n, m;
+    cin >> n >> m;
+
+    if(m % n != 0){
+        cout << -1 << endl;
+        return 0;
+    }
+    int x = m / n;
+
+    int ans = 0;
+    while(x % 2 == 0){
+        ans++;
+        x /= 2;
+    }
+    while(x % 3 == 0){
+        ans++;
+        x /= 3;
+    }
+
+    if(x != 1){
+        cout << -1 << endl;
+        return 0;
+    }
+    cout << ans << endl;
+
+    return 0;
+}
---
+++ CPP/Codeforces/1141/c.cpp
@@ -0,0 +1,79 @@
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#include <stack>
+#include <tuple>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <utility>
+#include <iomanip>
+
+#define int long long int
+#define rep(i, n) for(int i = 0; i < (n); ++i)
+#define ALL(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define UNIQUE(v) v.erase( unique(v.begin(), v.end()), v.end() )
+
+using namespace std;
+
+typedef pair<int, int> P;
+
+const int INF = 1e15;
+const int MOD = 1e9+7;
+
+template <typename T>
+using vector2 = vector<vector<T>>;
+template <typename T>
+vector2<T> initVec2(size_t n0, size_t n1, T e = T()){
+    return vector2<T>(n0, vector<T>(n1, e));
+}
+
+template <typename T>
+using vector3 = vector<vector<vector<T>>>;
+template <typename T>
+vector3<T> initVec3(size_t n0, size_t n1, size_t n2, T e = T()){
+    return vector3<T>(n0, vector2<T>(n1, vector<T>(n2, e)));
+}
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    vector<int> q(n-1);
+    rep(i, n - 1) cin >> q[i];
+
+    int mini = 0;
+    int x = 0;
+    rep(i, n - 1){
+        x += q[i];
+        mini = min(mini, x);
+    }
+
+    vector<int> p = {1 - mini};
+    rep(i, n - 1){
+        p.push_back(p.back() + q[i]);
+    }
+
+    vector<int> s = p;
+    sort(ALL(s));
+    for(int i = 1; i <= n; i++){
+        if(i != s[i-1]){
+            cout << -1 << endl;
+            return 0;
+        }
+    }
+
+    rep(i, n){
+        cout << p[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
