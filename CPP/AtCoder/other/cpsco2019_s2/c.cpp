78245e6a25d0b65f9e5e37b2174bfce723de3acb
kkisic <amareamo.pxhxc@gmail.com>
2019-05-08 00:24:34 +0900

    atcoder cpsco2019_s2 c C++

---
+++ CPP/AtCoder/other/cpsco2019_s2/c.cpp
@@ -0,0 +1,68 @@
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#include <stack>
+#include <tuple>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <utility>
+#include <iomanip>
+
+#define int long long int
+#define rep(i, n) for(int i = 0; i < (n); ++i)
+#define ALL(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define UNIQUE(v) v.erase( unique(v.begin(), v.end()), v.end() )
+
+using namespace std;
+
+typedef pair<int, int> P;
+
+const int INF = 1e15;
+const int MOD = 1e9+7;
+
+template <typename T>
+using vector2 = vector<vector<T>>;
+template <typename T>
+vector2<T> initVec2(size_t n0, size_t n1, T e = T()){
+    return vector2<T>(n0, vector<T>(n1, e));
+}
+
+template <typename T>
+using vector3 = vector<vector<vector<T>>>;
+template <typename T>
+vector3<T> initVec3(size_t n0, size_t n1, size_t n2, T e = T()){
+    return vector3<T>(n0, vector2<T>(n1, vector<T>(n2, e)));
+}
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n, k;
+    string s;
+    cin >> n >> k >> s;
+
+    vector<P> v(n);
+    int d = 0;
+    rep(i, n){
+        if(s[i] == '('){
+            d++;
+        }else{
+            d--;
+        }
+        v[i] = {d, i};
+    }
+    sort(ALL(v), greater<P>());
+
+    int ans = 0;
+    rep(i, k) ans += v[i].first;
+    cout << ans << endl;
+
+    return 0;
+}
