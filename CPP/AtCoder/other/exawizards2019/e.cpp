4c2729ce3506b1e8933b5d1d309c40c3a4e7ec98
kkisic <amareamo.pxhxc@gmail.com>
2019-05-13 23:00:00 +0900

    atcoder exawizards2019 e C++

---
+++ CPP/AtCoder/other/exawizards2019/e.cpp
@@ -0,0 +1,114 @@
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#include <stack>
+#include <tuple>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <utility>
+#include <iomanip>
+
+#define int long long int
+#define rep(i, n) for(int i = 0; i < (n); ++i)
+#define ALL(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define UNIQUE(v) v.erase( unique(v.begin(), v.end()), v.end() )
+
+using namespace std;
+
+typedef pair<int, int> P;
+
+const int INF = 1e15;
+const int MOD = 1e9+7;
+
+template <typename T>
+using vector2 = vector<vector<T>>;
+template <typename T>
+vector2<T> initVec2(size_t n0, size_t n1, T e = T()){
+    return vector2<T>(n0, vector<T>(n1, e));
+}
+
+template <typename T>
+using vector3 = vector<vector<vector<T>>>;
+template <typename T>
+vector3<T> initVec3(size_t n0, size_t n1, size_t n2, T e = T()){
+    return vector3<T>(n0, vector2<T>(n1, vector<T>(n2, e)));
+}
+
+int powM(int x, int n){
+    if(n == 0){
+        return 1;
+    }
+    if(n % 2 == 0){
+        return powM(x * x % MOD, n / 2);
+    }
+    return powM(x, n-1) * x % MOD;
+}
+
+int factM(int n){
+    static vector<int> t = {1};
+
+    if((int)t.size() > n){
+        return t[n];
+    }
+
+    for(int i = t.size(); i <= n; i++){
+        t.push_back(t[i-1] * i % MOD);
+    }
+    return t[n];
+}
+
+class Combination{
+    private:
+        vector<int> t;
+
+    public:
+        Combination(int n): t(n+1){
+            t[n] = powM(factM(n), MOD-2);
+
+            for(int i = n-1; i >= 0; i--){
+                t[i] = t[i+1] * (i+1) % MOD;
+            }
+        }
+
+        int comb(int n, int k){
+            if(k == 0){
+                return 1;
+            }
+            if(n < k || n <= 0 || k < 0){
+                return 0;
+            }
+            return factM(n) * t[k] % MOD * t[n-k] % MOD;
+        }
+
+        int getInv(int n){
+            return t[n];
+        }
+};
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int b, w;
+    cin >> b >> w;
+
+    Combination c(b + w);
+    int pb = 1;
+    int pbw = 1;
+    for(int i = 1; i <= b + w; i++){
+        int ans = (pb - pbw * powM(2, MOD - 2) % MOD + MOD) % MOD;
+        cout << ans * powM(powM(2, MOD - 2), i - 1) % MOD << endl;
+
+        pb = (2 * pb % MOD + MOD - c.comb(i - 1, b - 1)) % MOD;
+        pbw = (2 * pbw % MOD + MOD - c.comb(i - 1, b - 1)) % MOD;
+        pbw = (pbw + MOD - c.comb(i - 1, i - w)) % MOD;
+    }
+
+    return 0;
+}
