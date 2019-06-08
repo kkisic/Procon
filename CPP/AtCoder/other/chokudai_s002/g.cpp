02d747eed432d160fb0ba08e2052f29bfb2ed49d
kkisic <amareamo.pxhxc@gmail.com>
2019-06-08 23:37:00 +0900

    atcoder chokudai_s002 g C++

---
+++ CPP/AtCoder/other/chokudai_s002/g.cpp
@@ -0,0 +1,69 @@
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#include <stack>
+#include <tuple>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <utility>
+#include <iomanip>
+
+#define int long long int
+#define rep(i, n) for(int i = 0; i < (n); ++i)
+#define ALL(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define UNIQUE(v) v.erase( unique(v.begin(), v.end()), v.end() )
+
+using namespace std;
+
+typedef pair<int, int> P;
+
+const int INF = 1e15;
+const int MOD = 1e9+7;
+
+template <typename T>
+using vector2 = vector<vector<T>>;
+template <typename T>
+vector2<T> initVec2(size_t n0, size_t n1, T e = T()){
+    return vector2<T>(n0, vector<T>(n1, e));
+}
+
+template <typename T>
+using vector3 = vector<vector<vector<T>>>;
+template <typename T>
+vector3<T> initVec3(size_t n0, size_t n1, size_t n2, T e = T()){
+    return vector3<T>(n0, vector2<T>(n1, vector<T>(n2, e)));
+}
+
+int gcd(int a, int b){
+    if(a < b){
+        swap(a, b);
+    }
+    int r = a % b;
+    while(r != 0){
+        a = b;
+        b = r;
+        r = a % b;
+    }
+    return b;
+}
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    rep(i, n){
+        int a, b;
+        cin >> a >> b;
+        cout << gcd(a, b) << endl;
+    }
+
+    return 0;
+}
