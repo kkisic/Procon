2034dac1f101847f6c6101d574144b92eb08dbf8
kkisic <amareamo.pxhxc@gmail.com>
2019-08-24 22:56:55 +0900

    atcoder jsc2019-Q abd C++

---
+++ CPP/AtCoder/other/jsc2019-Q/b.cpp
@@ -0,0 +1,82 @@
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#include <stack>
+#include <tuple>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <utility>
+#include <iomanip>
+
+#define int long long int
+#define rep(i, n) for(int i = 0; i < (n); ++i)
+#define ALL(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define UNIQUE(v) v.erase( unique(v.begin(), v.end()), v.end() )
+
+using namespace std;
+
+typedef pair<int, int> P;
+
+const int INF = 1e15;
+const int MOD = 1e9+7;
+
+template <typename T>
+using vector2 = vector<vector<T>>;
+template <typename T>
+vector2<T> initVec2(size_t n0, size_t n1, T e = T()){
+    return vector2<T>(n0, vector<T>(n1, e));
+}
+
+template <typename T>
+using vector3 = vector<vector<vector<T>>>;
+template <typename T>
+vector3<T> initVec3(size_t n0, size_t n1, size_t n2, T e = T()){
+    return vector3<T>(n0, vector2<T>(n1, vector<T>(n2, e)));
+}
+
+int powM(int x, int n){
+    if(n == 0){
+        return 1;
+    }
+    if(n % 2 == 0){
+        return powM(x * x % MOD, n / 2);
+    }
+    return powM(x, n-1) * x % MOD;
+}
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    rep(i, n) cin >> a[i];
+
+    int ans = 0;
+    vector<int> sum(2001);
+    rep(i, n){
+        sum[a[i]]++;
+    }
+
+    int half = powM(2, MOD - 2);
+
+    vector<int> cnt(2001);
+    for(int i = n - 1; i >= 0; i--){
+        cnt[a[i]]++;
+        for(int j = 1; j < a[i]; j++){
+            int last = (cnt[j] + sum[j] * (k - 1) % MOD) % MOD;
+            ans += (cnt[j] + last) % MOD * k % MOD * half % MOD;
+            ans %= MOD;
+        }
+    }
+    cout << ans << endl;
+
+    return 0;
+}
