f6cfcd295d53415abb86eb77ba1c144d5750fb45
kkisic <amareamo.pxhxc@gmail.com>
2019-09-30 00:03:59 +0900

    atcoder jsc2019-F a C++

---
+++ CPP/AtCoder/other/jsc2019-F/a.cpp
@@ -0,0 +1,69 @@
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#include <stack>
+#include <tuple>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <utility>
+#include <iomanip>
+
+#define int long long int
+#define rep(i, n) for(int i = 0; i < (n); ++i)
+#define ALL(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define UNIQUE(v) v.erase( unique(v.begin(), v.end()), v.end() )
+
+using namespace std;
+
+typedef pair<int, int> P;
+
+const int INF = 1e15;
+const int MOD = 1e9+7;
+
+template <typename T>
+using vector2 = vector<vector<T>>;
+template <typename T>
+vector2<T> initVec2(size_t n0, size_t n1, T e = T()){
+    return vector2<T>(n0, vector<T>(n1, e));
+}
+
+template <typename T>
+using vector3 = vector<vector<vector<T>>>;
+template <typename T>
+vector3<T> initVec3(size_t n0, size_t n1, size_t n2, T e = T()){
+    return vector3<T>(n0, vector2<T>(n1, vector<T>(n2, e)));
+}
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n), b(m);
+    rep(i, n) cin >> a[i];
+    rep(i, m) cin >> b[i];
+
+    P e = {-1, -1};
+    vector<P> used(2000001, e);
+    rep(i, n){
+        rep(j, m){
+            P x = {i, j};
+            int s = a[i] + b[j];
+            if(used[s] != e){
+                cout << used[s].first << " " << used[s].second << " " << i << " " << j << endl;
+                return 0;
+            }
+            used[s] = x;
+        }
+    }
+    cout << -1 << endl;
+
+    return 0;
+}
